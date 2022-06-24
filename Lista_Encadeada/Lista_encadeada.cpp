12d39cca6442ae7cc340f2cef094845f4fdcaaef
Jão <91478592+Jaum1981@users.noreply.github.com>
2022-06-24 09:10:30 -0300

    Update Lista_encadeada.cpp

--- Lista_Encadeada/Lista_encadeada.cpp
+++ Lista_Encadeada/Lista_encadeada.cpp
@@ -1,102 +1,71 @@
 #include <iostream>
 #include <stdlib.h>
+#include <cassert>
 
-using namespace std;
 
-typedef struct node{
+class ListNode{
 
-    int valor;
-    struct node *proximo;
+public:
 
-}Node;
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr){
+    }
+    ListNode(int val, ListNode *next) : val(val), next(next){
+    }
+    ListNode(int val, ListNode *next){
+        this -> val = val;
+        this -> next = next;
+    }
+};
 
-typedef struct{
-
-    node *inicio;
-    int tam;
-}Lista;
-
-//inserção no inicio da lista
-void inserirInicio(Lista *lista, int valor){ 
-
-    node *novo = (node*)malloc(sizeof(node));
-    novo->valor = valor; // (*novo).valor = valor;
-    novo->proximo = lista->inicio;
-    lista->inicio = novo;
-    lista->tam++;
-
-}
-
-//inserir no final da lista
-void inserirFim(Lista *lista, int valor){
-
-    Node *no, *novo = (node*)malloc(sizeof(node));
-    novo -> valor = valor;
-    novo -> proximo = NULL;
-
-    if(lista -> inicio == NULL){
-        lista -> inicio = novo;
+ListNode *insertback(ListNode *head, int val){
+    if(head == nullptr){
+        return new ListNode;
     }else{
-        no = lista->inicio;
-        while(no->proximo != NULL)
-            no = no->proximo;
-        no->proximo = novo;
-    }
-    lista->tam++;
-
-}
-
-//imprimir a lista
-void inprimir(Lista *lista){
-    Node *inicio = lista -> inicio;
-    cout << "Tamanho da lista: "<< lista->tam << endl;
-    while(inicio != NULL){
-        cout << inicio -> valor << " ";
-        inicio = inicio -> proximo;
-    }
-        cout << endl << endl;
-}
-
-int main() {
-    
-    Lista lista;
-    int opcao, valor;
-
-    //começa como vazia
-    lista.inicio = NULL;
-    lista.tam = 0;
-
-    do{
-        cout << "1 - Inserir no inicio" << endl;
-        cout << "2 - Imprimir" << endl;
-        cout << "3 - Inserir no final" << endl;
-        cout << "5 - sair"<< endl;
-        cin >> opcao;
-        switch(opcao){
-            case 1:
-            cout << "digite um valor a ser iserido" << endl;
-            cin >> valor;
-            inserirInicio(&lista, valor);
-            break;
-
-            case 2:
-            inprimir(&lista);
-            break;
-
-            case 3:
-            cout << "digite um valor a ser iserido" << endl;
-            cin >> valor;
-            inserirFim(&lista, valor);
-            break;
-
-            case 5:
-            cout << "Saindo..." << endl;
-            break;
-
-            default:
-            cout << "Opcao invalida" << endl;
+        if(head -> next == nullptr){
+            head -> next = new ListNode;
+            return head;
+        }else{
+            insertback(head -> next, val);
+            return head;
         }
-    }while(opcao != 5);
+    }
+}
 
-    return 0;
+ListNode *merge(ListNode *head1, ListNode *head2){
+
+    if(head1 == nullptr){
+        return head2;
+    }
+    if(head2 == nullptr){
+        return head1;
+    }
+    if(head1 -> val < head2 -> val){
+        head1 -> next = merge(head1 -> next, head2);
+        return head1;
+    }else{
+        head2 -> next = merge(head1 -> next, head2);
+        return head2;
+    }
+
+}
+
+//pega o Nth numero a partir do fim da lista
+int getNthfromLast(ListNode *head, int n){
+
+    ListNode *fast = head;
+    ListNode *slow = head;
+
+    for(int i=1; i<=n-1; i++){
+        fast = fast -> next;
+    }
+
+    while(fast -> next != nullptr){
+        fast = fast -> next;
+        slow = slow -> next;
+    }
+
+    return slow -> val;
+
 }
